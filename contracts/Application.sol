a2bf73b7e5b3e28a4598157df97e358fe594d8f4
Bertie Spell <berniesnell@gmail.com>
2018-08-17 14:12:01 +0100

    Add method to submit an application

--- contracts/Application.sol
+++ contracts/Application.sol
@@ -5,19 +5,30 @@ contract Application {
 
     address public submissionAddress;
 
+    address public fundingApplicationAddress;
+
     string public applicationName;
+
+    string public description;
 
     uint public requestedFunds;
 
     constructor(
+        address _fundingApplication,
         address _submissionAddress,
         string _applicationName,
+        string _description,
         uint _requestedFunds
     ) public {
+        fundingApplicationAddress = _fundingApplication;
         submissionAddress = _submissionAddress;
         applicationName = _applicationName;
+        description = _description;
         requestedFunds = _requestedFunds;
     }
 
-    
+    // withdraw the funds - this can either be C level or the submission address!
+    // function withdrawFunds() public {
+
+    // }
 }
--- contracts/FundingApplications.sol
+++ contracts/FundingApplications.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.23;
 import "./AccessControl.sol";
 import "./SafeMath.sol";
+import "./Application.sol";
 
 
 contract FundingApplications is AccessControl {
@@ -8,6 +9,24 @@ contract FundingApplications is AccessControl {
 
     bool public applicationsOpen;
     bool public votingOpen;
+
+    Proposal[] public proposals;
+
+    struct Proposal {
+        address submissionAddress;
+        address fundingApplicationAddress;
+        string applicationName;
+        string description; 
+        uint requestedFunds;
+    }
+
+    event ApplicationSubmitted(
+        address submissionAddress,
+        address fundingApplicationAddress,
+        string applicationName,
+        string description,
+        uint requestedFunds
+    );
 
     constructor() public {
         applicationsOpen = false;
@@ -38,5 +57,33 @@ contract FundingApplications is AccessControl {
 
     function closeVoting() external onlyCLevel {
         votingOpen = false;       
+    }
+
+    function submitApplication(
+        string _applicationName, 
+        string _description, 
+        uint _requestedFunds
+    ) public {
+        Proposal memory proposal;
+        proposal.submissionAddress = msg.sender;
+        proposal.applicationName = _applicationName;
+        proposal.description = _description;
+        proposal.requestedFunds = _requestedFunds;
+        proposal.fundingApplicationAddress = new Application(
+            this,
+            msg.sender,
+            _applicationName,
+            _description,
+            _requestedFunds
+        );
+        proposals.push(proposal);
+
+        emit ApplicationSubmitted(
+            msg.sender,
+            proposal.fundingApplicationAddress,
+            _applicationName,
+            _description,
+            _requestedFunds
+        );
     }
 }
