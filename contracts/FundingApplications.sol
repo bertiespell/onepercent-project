b4a17b2e14453f30866ed8295c32ad02e908fba4
Bertie Spell <berniesnell@gmail.com>
2018-08-17 11:58:44 +0100

    Add tests and code to open and close funding proposals and voting

--- contracts/FundingApplications.sol
+++ contracts/FundingApplications.sol
@@ -1,9 +1,42 @@
 pragma solidity ^0.4.23;
+import "./AccessControl.sol";
+import "./SafeMath.sol";
 
 
-contract FundingApplications {
-    
+contract FundingApplications is AccessControl {
+    using SafeMath for uint256;
+
+    bool public applicationsOpen;
+    bool public votingOpen;
+
     constructor() public {
-        
+        applicationsOpen = false;
+        votingOpen = false;
+    }
+
+    modifier applicationsClosed() {
+        require(applicationsOpen == false);
+        _;
+    }
+
+    modifier votingClosed() {
+        require(votingOpen == false);
+        _;
+    }
+
+    function openApplications() external onlyCLevel votingClosed {
+        applicationsOpen = true;
+    }
+
+    function closeApplications() external onlyCLevel {
+        applicationsOpen = false;       
+    }
+
+    function openVoting() external onlyCLevel applicationsClosed {
+        votingOpen = true;
+    }
+
+    function closeVoting() external onlyCLevel {
+        votingOpen = false;       
     }
 }
--- test/FundingApplications.spec.js
+++ test/FundingApplications.spec.js
@@ -6,6 +6,7 @@ contract('FundingApplications', function(accounts) {
     const alice = accounts[1];
     const bob = accounts[2];
 
+    let fundingApplication;
     beforeEach(async () => {
         fundingApplication = await FundingApplications.new();
     });
@@ -24,19 +25,131 @@ contract('FundingApplications', function(accounts) {
     it("once applications close, the last set of proposals should be open to be voted on - i.e. voting should be open", async () => {
        
     });
-    it("any account should be able to spend OPC tokens voting for a proposal", async () => {
+    it("any account should be able to spend OPC tokens voting for a proposal - when voting is open", async () => {
        
     });
     it("when voting closes - users should no longer be able to submit votes", async () => {
        
     });
-    it("only c level accounts can open and close application and voting windows", async () => {
-       
+    it("only c level accounts can open and close applications", async () => {
+        await fundingApplication.setCEO(accounts[1], {from: owner});
+        await fundingApplication.setCOO(accounts[2], {from: accounts[1]});
+        await fundingApplication.setCFO(accounts[3], {from: accounts[1]});
+
+        let applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, false);      
+        await fundingApplication.openApplications({from: accounts[1]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, true);
+        await fundingApplication.closeApplications({from: accounts[1]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, false);
+
+        await fundingApplication.openApplications({from: accounts[2]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, true);
+        await fundingApplication.closeApplications({from: accounts[2]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, false);
+
+        await fundingApplication.openApplications({from: accounts[3]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, true);
+        await fundingApplication.closeApplications({from: accounts[3]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, false);
+
+        try {
+            await fundingApplication.openApplications({from: accounts[4]});
+        } catch(e) {
+            // do nothing - catch the VM exception we expect here
+        }
+
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, false);
+
+        await fundingApplication.openApplications({from: accounts[3]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, true);
+
+        try {
+            await fundingApplication.closeApplications({from: accounts[4]});
+        } catch(e) {
+            // do nothing - catch the VM exception we expect here
+        }
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, true);
     });
-    it("should not allow a c level account to open voting if applications are still open (applications should close first)", async () => {
-       
+    it("only c level accounts can open and close voting", async () => {
+        await fundingApplication.setCEO(accounts[1], {from: owner});
+        await fundingApplication.setCOO(accounts[2], {from: accounts[1]});
+        await fundingApplication.setCFO(accounts[3], {from: accounts[1]});
+
+        let votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);      
+        await fundingApplication.openVoting({from: accounts[1]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, true);
+        await fundingApplication.closeVoting({from: accounts[1]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);
+
+        await fundingApplication.openVoting({from: accounts[2]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, true);
+        await fundingApplication.closeVoting({from: accounts[2]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);
+
+        await fundingApplication.openVoting({from: accounts[3]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, true);
+        await fundingApplication.closeVoting({from: accounts[3]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);
+
+        try {
+            await fundingApplication.openVoting({from: accounts[4]});
+        } catch(e) {
+            // do nothing - catch the VM exception we expect here
+        }
+
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);
+
+        await fundingApplication.openVoting({from: accounts[3]});
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, true);
+
+        try {
+            await fundingApplication.closeVoting({from: accounts[4]});
+        } catch(e) {
+            // do nothing - catch the VM exception we expect here
+        }
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, true);
     });
-    it("should not allow a c level account to open applications/proposals if voting is still open (voting should close first)", async () => {
+    it("should only allow a c level account to open voting if applications are closed (applications should close first)", async () => {
+
+        await fundingApplication.openApplications({from: accounts[0]});
+        applicationsOpen = await fundingApplication.applicationsOpen();
+        assert.equal(applicationsOpen, true);
+
+        let votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);
+        
+        let error;
+        try {
+            await fundingApplication.openVoting({from: accounts[0]});
+        } catch(e) {
+            // do nothing - catch the VM exception we expect here
+            error = e;
+        }
+        assert.notEqual(error, undefined);
+        votingOpen = await fundingApplication.votingOpen();
+        assert.equal(votingOpen, false);
+    });
+    it("should only allow a c level account to open applications/proposals when voting is closed (voting should close first)", async () => {
        
     });
     it("when voting closes - the correct last winner should be emitted", async () => {
